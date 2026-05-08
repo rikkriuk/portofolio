3da2daa140ef8361d792fad5e29bef4fda9cf00e
ikokywu <rikiridwan949@gmail.com>
2026-05-08 19:06:55 +0700

    fist commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-zinc-900">
+      <h1 className="text-5xl font-bold text-cyan-400">
+        React + TS + Tailwind v4
+      </h1>
+    </div>
+  )
+}
