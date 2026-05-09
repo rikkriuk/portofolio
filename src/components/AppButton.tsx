8b66e4f2192500a7e5f8067c98577d0ba05da19f
ikokywu <rikiridwan949@gmail.com>
2026-05-09 20:13:04 +0700

    refactor: adjust app button

--- src/components/AppButton.tsx
+++ src/components/AppButton.tsx
@@ -1,28 +1,48 @@
 
 type AppButtonProps = {
-   label: string;
-   type?: "primary" | "secondary";
+   label: string | React.ReactNode;
+   type?: "primary" | "secondary" | "tertiary";
    onClick: () => void;
+   className?: string;
+   link?: string;
 }
 
 const AppButton = (props: AppButtonProps) => {
-   const { label, type = "primary", onClick } = props;
-   let className = "border-2 border-[#1C1B1A] py-2.5 px-3.5 font-bold text-base cursor-pointer transition-all duration-200";
+   const { label, type = "primary", onClick, className, link } = props;
+   let buttonClassName = "border-2 border-primary py-2.5 px-3.5 font-bold text-base cursor-pointer transition-all duration-200";
 
    switch (type) {
       case "primary":
-         className += " bg-[#F1EDEA]";
+         buttonClassName += " bg-[#F1EDEA]";
          break;
       case "secondary":
-         className += " bg-[#FE00FE] text-[#500050] shadow-[6px_6px_0_0_rgba(0,0,0,1)] active:translate-x-[6px] active:translate-y-[6px] active:shadow-none";
+         buttonClassName += " bg-secondary text-[#500050] shadow-[6px_6px_0_0_rgba(0,0,0,1)] active:translate-x-[6px] active:translate-y-[6px] active:shadow-none";
+         break;
+      case "tertiary":
+         buttonClassName += " bg-[#FE00FE] border-4 border-primary";
          break;
       default:
          break;
    }
 
+   if (className) {
+      buttonClassName += ` ${className}`;
+   }
+
+   if (link) {
+      return (
+         <a 
+            href={link} 
+            className={buttonClassName}
+         >
+            {label}
+         </a>
+      );
+   }
+
    return (
       <button
-         className={className}
+         className={buttonClassName}
          onClick={onClick}
       >
          {label}
