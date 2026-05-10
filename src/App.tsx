b5a15affefb9fda7e20d5d61c3bf23498f8c8676
ikokywu <rikiridwan949@gmail.com>
2026-05-10 21:48:21 +0700

    refactor: adjust style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import AppNavbar from "./layouts/AppNavbar";
 import HomePage from "./screens/home"
 
 const App = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
-    <main className="bg-[#E6E2DF]">
-      <AppNavbar />
-      <HomePage />
+    <main className="bg-[#FDF8F5] overflow-hidden">
+      <AppNavbar
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+
+      <div
+        className={`
+          px-8 lg:px-24 xl:px-48 space-y-16 transition-all duration-300 overflow-hidden
+          ${isSidebarOpen ? "blur-sm pointer-events-none" : ""}
+        `}
+      >
+        <HomePage />
+      </div>
     </main>
   )
 }
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,9 +1,13 @@
 import HeroSection from "./components/HeroSection"
+import ProjectSection from "./components/ProjectSection"
+import StackSection from "./components/StackSection"
 
 const HomePage = () => {
    return (
       <>
          <HeroSection />
+         <StackSection />
+         <ProjectSection />
       </>
    )
 }
